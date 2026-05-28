8fc393444b2af651b609a03d319ca805bd5f5311
Luciana A, Ricardo R <restreporicardo28@gmail.com>
2026-05-28 17:25:11 -0500

    get_user_information : Add inputs and style

---
+++ Restaurante/js/inventario.js
@@ -0,0 +1,5 @@
+let producto = document.getElementById("producto").value;
+let codigo = document.getElementById("codigo").value;
+let nombre = document.getElementById("nombre").value;
+let cantidad = document.getElementById("cantidad").value;
+let marca = document.getElementById("marca").value; 
---
+++ Restaurante/js/usuario.js
@@ -0,0 +1,5 @@
+let nombre = document.getElementByName("nombre").value;
+let apellido = document.getElementByName("apellido").value;
+let correo = document.getElementByName("correo").value;
+let contraseña = document.getElementById("contraseña").value;
+let numero_documento = document.getElementById("numero_documento").value;
